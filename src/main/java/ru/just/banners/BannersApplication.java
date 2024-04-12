9511354502f7d34e1e6de53404b827cf0e1cc1e6
Kirill_Porozov <vtb4102627@region.vtb.ru>
2024-04-12 07:52:08 +0300

    feature [banners]: add simple project

---
+++ src/main/java/ru/just/banners/BannersApplication.java
@@ -0,0 +1,13 @@
+package ru.just.banners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BannersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BannersApplication.class, args);
+    }
+
+}
