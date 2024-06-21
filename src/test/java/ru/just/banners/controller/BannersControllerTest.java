eb47a9713769e3d8efa0c2d50ac3ba3c344ec046
JustBlood <msg.msgq@mail.ru>
2024-06-21 16:25:52 +0300

    feat [banner]: add tests and e2e test

--- src/test/java/ru/just/banners/controller/BannersControllerTest.java
+++ src/test/java/ru/just/banners/controller/BannersControllerTest.java
@@ -13,22 +13,21 @@ import org.springframework.context.MessageSource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.LinkedMultiValueMap;
 import ru.just.banners.controller.advice.ErrorMessage;
 import ru.just.banners.dto.BannerDto;
 import ru.just.banners.dto.BannerIdDto;
 import ru.just.banners.dto.CreateBannerDto;
+import ru.just.banners.dto.PatchBannerDto;
+import ru.just.banners.schedule.DelayDeletingService;
 
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Locale;
 import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -43,6 +42,8 @@ class BannersControllerTest {
     private ObjectMapper objectMapper;
     @Autowired
     private MessageSource messageSource;
+    @Autowired
+    private DelayDeletingService delayDeletingService;
     final String adminToken = "admin-token";
 
     @SneakyThrows
@@ -128,5 +129,97 @@ class BannersControllerTest {
         ErrorMessage errorMessage = objectMapper.readValue(response, ErrorMessage.class);
         assertNotNull(errorMessage.getError());
         assertEquals(expectedMessage, errorMessage.getError());
+    }
+
+    @SneakyThrows
+    @Test
+    void deleteBanner_whenNoBannerExists_statusNotFound() {
+        long bannerId = 999;
+        String expectedMessage = messageSource.getMessage(
+                "error.bannerNotFound", null, Locale.of("ru_RU"));
+
+        String response = mockMvc.perform(delete("/api/v1/banner/" + bannerId)
+                        .header(HttpHeaders.CONTENT_TYPE, "application/json")
+                        .header("X-Auth-Key", adminToken))
+                .andExpect(status().isNotFound())
+                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+        assertNotNull(response);
+        ErrorMessage errorMessage = objectMapper.readValue(response, ErrorMessage.class);
+        assertNotNull(errorMessage.getError());
+        assertEquals(expectedMessage, errorMessage.getError());
+    }
+
+    @SneakyThrows
+    @Test
+    void deleteBanner_whenBannerExists_statusNoContent() {
+        long bannerId = 1;
+
+        String response = mockMvc.perform(delete("/api/v1/banner/" + bannerId)
+                        .header(HttpHeaders.CONTENT_TYPE, "application/json")
+                        .header("X-Auth-Key", adminToken))
+                .andExpect(status().isNoContent())
+                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+        assertNotNull(response);
+        assertEquals("", response);
+    }
+
+    @SneakyThrows
+    @Test
+    void patchBanner_whenBannerExists_statusOk() {
+        long bannerId = 1;
+        final long featureId = 99L;
+        final List<Long> tagIds = List.of(99L, 100L, 101L);
+        PatchBannerDto patchBannerDto = new PatchBannerDto();
+        patchBannerDto.setContent("{}");
+        patchBannerDto.setFeatureId(featureId);
+        patchBannerDto.setTagIds(tagIds);
+        patchBannerDto.setIsActive(false);
+
+        String response = mockMvc.perform(patch("/api/v1/banner/" + bannerId)
+                        .header(HttpHeaders.CONTENT_TYPE, "application/json")
+                        .header("X-Auth-Key", adminToken)
+                        .content(objectMapper.writeValueAsString(patchBannerDto)))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+        assertNotNull(response);
+        assertEquals("", response);
+        String getContent = mockMvc.perform(get("/api/v1/banner")
+                        .param("tag_id", String.valueOf(tagIds.getFirst()))
+                        .param("feature_id", String.valueOf(featureId))
+                        .header("X-Auth-Key", adminToken))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+        List<BannerDto> banner = objectMapper.readValue(getContent, new TypeReference<>() {});
+        assertEquals(featureId, banner.getFirst().getFeatureId());
+        assertEquals(tagIds, banner.getFirst().getTagIds());
+        assertEquals(bannerId, banner.getFirst().getBannerId());
+    }
+
+    @SneakyThrows
+    @ParameterizedTest
+    @CsvSource({
+            "1,",
+            ",1"
+    })
+    void deleteBannerByFeatureOrTag_whenBannerExists_statusNoContent(Long featureId, Long tagId) {
+        LinkedMultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        if (featureId != null) params.put("feature_id", List.of(featureId.toString()));
+        if (tagId != null) params.put("tag_id", List.of(tagId.toString()));
+        mockMvc.perform(delete("/api/v1/banner")
+                        .params(params)
+                        .header(HttpHeaders.CONTENT_TYPE, "application/json")
+                        .header("X-Auth-Key", adminToken))
+                .andExpect(status().isNoContent())
+                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+        delayDeletingService.deleteBannersByFlag();
+        String getContent = mockMvc.perform(get("/api/v1/banner")
+                        .params(params)
+                        .header("X-Auth-Key", adminToken))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+        List<BannerDto> banners = objectMapper.readValue(getContent, new TypeReference<>() {});
+        assertTrue(banners.isEmpty());
     }
 }
